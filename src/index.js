4e3c91bd8182a3fc9b7d27599cb6e73e04cccef9
James <admin@James.local>
2020-03-03 22:49:09 +0300

    Restructure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/styling/index.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./components/App";
+import Heading from "./components/common/Heading";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <Heading />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
